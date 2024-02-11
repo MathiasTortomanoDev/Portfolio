218b8d0195f4b86db53c09d9daa10d477b3bfc06
mathias <mathiastortomano.dev@gmail.com>
2024-02-10 22:11:27 -0300

    Se implementaron etiquetas meta, modo oscuro en localstorage y se solucionaron detalles visuales menores (background mas blanco en el modo claro, efecto hover en la seccion de preguntas y el box shadow de las skills en modo calro).

--- js/darkMode.js
+++ js/darkMode.js
@@ -1,14 +1,23 @@
 
 let darkModeSwitch = document.querySelector(".switch__checkbox")
 
+// light mode in localStorage
+if (localStorage.getItem("dark-mode") === "false") {
+    document.body.classList.add("lightMode");
+    darkModeSwitch.checked = false;
+}
+
+
 const IsDark = () => {
     let darkModeSwitchCheck = document.querySelector(".switch__checkbox")
 
     if (darkModeSwitchCheck.checked == true) {
         document.body.classList.remove("lightMode")   
+        localStorage.setItem("dark-mode", "true")
     } 
     else {
         document.body.classList.add("lightMode")   
+        localStorage.setItem("dark-mode", "false")
     } 
 
 }
